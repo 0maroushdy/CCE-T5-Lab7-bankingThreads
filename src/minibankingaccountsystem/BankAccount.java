10914be0065b13a3686dfdbd091a38bfd7ff9155
Omar <Omar.roushdy@outlook.com>
2024-11-21 23:09:30 +0200

    finshing Banking Account class

--- src/minibankingaccountsystem/BankAccount.java
+++ src/minibankingaccountsystem/BankAccount.java
@@ -1,20 +1,79 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package minibankingaccountsystem;
+import java.util.*;
+import java.io.*;
 
 /**
  *
  * @author Omar
  */
-public class BankAccount {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class BankAccount {
+    private String id;
+    private double balance;
+    private ArrayList <String> transactionLog;
+    
+    // the constructor -----------------------------
+    public BankAccount(String id, double balance){
+        this.id = id;
+        this.balance = balance;
+        this.transactionLog = new ArrayList<>();
+        transactionLog.add("Account created with balance: " + balance);
+
+    }
+    
+    // adding balence method -----------------------------
+    public void Deposit(double amount)throws InvalidTransactionException{
+        if (amount <= 0) {
+            throw new InvalidTransactionException("Deposit failed: Amount must be greater than 0");
+        }
+        
+        try{
+            balance += amount;
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+        String logEntry = "Deposited: +" + amount + ", New Balance: " + balance;
+        transactionLog.add(logEntry);
+        System.out.println(logEntry);
+    }
+    
+    // withdrawing balance method -----------------------------
+    public void Withdraw(double amount)throws InvalidTransactionException{
+        if (amount <= 0) {
+            throw new InvalidTransactionException("Withdraw failed: Amount must be greater than 0");
+        } else if (amount < balance){
+            throw new InvalidTransactionException("Withdraw failed: مفيش رصيد كفاية يا صديقي");
+        }
+            
+        try{
+            balance -= amount;
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+        String logEntry = "Withdrawed: -" + amount + ", New Balance: " + balance;
+        transactionLog.add(logEntry);
+        System.out.println(logEntry);   
+    }
+    
+    
+    //get current balance -----------------------------
+    public double getBalance() {
+        return balance;
+    }
+
+    // print transaction log -----------------------------
+    public void printTransactionLog() {
+        System.out.println("Transaction Log for Account ID: " + id);
+        for (String log : transactionLog) {
+            System.out.println(log);
+        }
+    }
+
+    // Getter for account ID -----------------------------
+    public String getId() {
+        return id;
     }
     
 }
